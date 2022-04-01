b4b65eccccd991d4a510d5b03559dafd6147de25
aravindlingam <99788432+aravindlingam@users.noreply.github.com>
2022-04-01 17:10:49 +0530

    Add files via upload

---
+++ 1.6SessionTrackingUsingCookirs/src/main/java/com/Dashboard.java
@@ -0,0 +1,59 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Dashboard
+ */
+public class Dashboard extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Dashboard() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Welcome to Dashboard</h1>");
+		
+				HttpSession session =	request.getSession(false);
+		
+				String uname = (String)	session.getAttribute("uname");
+				
+				if(uname == null ) {
+					
+					out.print("No Session found for the user "+uname);
+				}
+				else {
+					
+					out.print("<h1>Welcome "+uname+"</h1>");
+                    out.println("<a href='LogoutServlet'>Logout of session</a><br>");
+					
+				}
+}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
